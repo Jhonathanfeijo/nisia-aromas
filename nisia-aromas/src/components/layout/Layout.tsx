be6020f0596f61c214493bb83bfe8ac8de903a74
Jhonathan Feijo <jhonathanfeijomoraes@gmail.com>
2024-10-15 12:41:28 -0400

    creating state headerOption

--- nisia-aromas/src/components/layout/Layout.tsx
+++ nisia-aromas/src/components/layout/Layout.tsx
@@ -1,15 +1,22 @@
 import Header from "../header/Header";
 
-
-const Layout = ({ children }: { children: React.ReactNode }) => {
-    return (<>
-        <div className="w-screen h-screen flex flex-col" >
-            <Header />
-            <div className="max-[340px]:px-2 px-5 xl:px-[4rem] 2xl:px-[15rem] flex-1 w-full">
-                {children}
-            </div>
+const Layout = ({
+  children,
+  setHeaderOption,
+}: {
+  children: React.ReactNode;
+  setHeaderOption: React.Dispatch<React.SetStateAction<string>>;
+}) => {
+  return (
+    <>
+      <div className="w-screen h-screen flex flex-col">
+        <Header setHeaderOption={setHeaderOption} />
+        <div className="max-[340px]:px-2 px-5 xl:px-[4rem] 2xl:px-[15rem] flex-1 w-full">
+          {children}
         </div>
-    </>);
-}
+      </div>
+    </>
+  );
+};
 
 export default Layout;
--- nisia-aromas/src/pages/Home.tsx
+++ nisia-aromas/src/pages/Home.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Layout from "../components/layout/Layout";
 
 const Home = () => {
-    return (<>
-        <Layout>
-            <div className="w-full h-full"></div>
-        </Layout>
-    </>  );
-}
- 
+  const [headerOption, setHeaderOption] = useState<string>("home");
+  return (
+    <>
+      <Layout setHeaderOption={setHeaderOption}>
+        <div className="w-full h-full"></div>
+      </Layout>
+    </>
+  );
+};
+
 export default Home;
